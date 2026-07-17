c7f03df0f1a1b1e095bcf5b71481e433c90f5945
benhaube <haube.ben@gmail.com>
2026-07-17 17:09:20 -0400

    moved out of docs directory

--- docs/assets/code/bash/install-helper-script.sh
+++
@@ -1,4 +0,0 @@
-git clone --depth 1 https://github.com/Guilouz/Creality-Helper-Script.git /usr/data/helper-script  # (1)!
-sh /usr/data/helper-script/helper.sh  # (2)!
- 
-git config --global http.sslVerify false  # (3)!

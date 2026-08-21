441d9054fbfc46264610416f811d8de793916582
benhaube <haube.ben@gmail.com>
2026-08-21 13:52:11 -0400

    fix(code): corrected router cert directory

--- includes/code/bash/deploy_cert.sh
+++ includes/code/bash/deploy_cert.sh
@@ -7,8 +7,8 @@ CERT_DIR="/etc/letsencrypt/live/rac3r4life.myaddr.dev"
 echo "Copying certificates to ASUS router..."
 
 # 1. Copy the fullchain to cert.pem and private key to key.pem
-scp "$CERT_DIR/fullchain.pem" "$ROUTER_USER@$ROUTER_IP:/jffs/cert.pem"  # (1)!
-scp "$CERT_DIR/privkey.pem" "$ROUTER_USER@$ROUTER_IP:/jffs/key.pem"
+scp "$CERT_DIR/fullchain.pem" "$ROUTER_USER@$ROUTER_IP:/jffs/.cert/cert.pem"  # (1)!
+scp "$CERT_DIR/privkey.pem" "$ROUTER_USER@$ROUTER_IP:/jffs/.cert/key.pem"
 
 echo "Restarting router web server to apply changes..."
 
